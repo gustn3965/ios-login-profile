d6d01af68a611f204b848caf617870f327beb58f
gustn3965 <gustn3965@gmail.com>
2022-01-03 16:10:45 +0900

    feat: LoginViewController 의 IBOutlet 바인딩 및 기능 구현

--- LoginProfile/LoginViewController.swift
+++ LoginProfile/LoginViewController.swift
@@ -7,12 +7,42 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTextField()
+        setupLoginButton()
+    }
+    
+    func setupTextField() {
+        idTextField.delegate = self
+    }
+    
+    func setupLoginButton() {
+        loginButton.isEnabled = false
+        loginButton.setTitleColor(.black, for: .disabled)
     }
 
+    @IBAction private func clickLoginButton(_ sender: Any) {
+        print("login")
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let text = textField.text else {
+            loginButton.isSelected = false
+            return
+        }
+        loginButton.isEnabled = text.count >= 5
+        loginButton.backgroundColor = text.count >= 5 ? .systemYellow : .systemGray5
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
