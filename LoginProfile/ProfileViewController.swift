f12f0d00891a14920edc9118b0effe66ad64d00f
gustn3965 <gustn3965@gmail.com>
2022-01-05 15:13:01 +0900

    feat: BottomButtonStackView 구현 및 배치

--- LoginProfile/ProfileViewController.swift
+++ LoginProfile/ProfileViewController.swift
@@ -14,9 +14,16 @@ final class ProfileViewController: UIViewController {
         return stackView
     }()
     
+    private lazy var bottomButtonStackView: BottomButtonStackView = {
+        let stackView: BottomButtonStackView = BottomButtonStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTopStackView()
+        setupBottomStackView()
     }
     
     func setupTopStackView() {
@@ -25,6 +32,15 @@ final class ProfileViewController: UIViewController {
             topButtonStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 22),
             topButtonStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
             topButtonStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 18)
+        ])
+    }
+    
+    func setupBottomStackView() {
+        view.addSubview(bottomButtonStackView)
+        NSLayoutConstraint.activate([
+            bottomButtonStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            bottomButtonStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            bottomButtonStackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -64)
         ])
     }
 }
---
+++ LoginProfile/View/BottomButtonStackView.swift
@@ -0,0 +1,83 @@
+//
+//  BottomButtonStackView.swift
+//  LoginProfile
+//
+//  Created by kakao on 2022/01/05.
+//
+
+import UIKit
+
+final class BottomButtonStackView: UIStackView {
+    private let chatButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setImage(UIImage(named: "chat"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let callButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setImage(UIImage(named: "call"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let quoteButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setImage(UIImage(named: "quote"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let chatLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "1:1 채팅"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        return label
+    }()
+    
+    private let callLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "통화하기"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        return label
+    }()
+    
+    private let quoteLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "카카오스토리"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("not implemented")
+    }
+    
+    private func setupView() {
+        addArrangedSubview(makeVerticalStackView(top: chatButton, bottom: chatLabel))
+        addArrangedSubview(makeVerticalStackView(top: callButton, bottom: callLabel))
+        addArrangedSubview(makeVerticalStackView(top: quoteButton, bottom: quoteLabel))
+        
+        axis = .horizontal
+        alignment = .bottom
+        distribution = .fillEqually
+    }
+    
+    private func makeVerticalStackView(top topView: UIView, bottom bottomView: UIView) -> UIStackView {
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [topView, bottomView])
+        stackView.axis = .vertical
+        stackView.spacing = 17
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }
+}
